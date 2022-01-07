c5c9519f218666bff209cb7774ce24b338a6e3f5
SimonSCC <simonsondergaardchristiansen@gmail.com>
2022-01-07 05:29:40 +0100

    Add project files.

---
+++ LogicLayer.cs/Hubs/MainHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+
+namespace LogicLayer.Hubs
+{
+    public class MainHub
+    {
+        public async Task SendMessage(string user, string msg)
+        {
+            await Clients.All.SendAsync("ReceiveMEssage", user, msg);
+        }
+    }
+}
---
+++ LogicLayer/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    public class MainHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
